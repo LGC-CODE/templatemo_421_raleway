d329279e8645ab229c9a3f594e60bb4855e701aa
LGC-CODE <luisconstante@yahoo.com>
2015-08-19 15:02:40 -0700

    made intro responsive and changed colors

--- js/templatemo_custom.js
+++ js/templatemo_custom.js
@@ -24,6 +24,19 @@ jQuery(document).ready(function($) {
 		return false;
 	});
 
+	$("#about-btn a.aboutbutton").click(function(){
+		var id =  $(this).attr('class');
+		id = id.split('-');
+		$('a.active').removeClass('active');
+    	$(this).addClass('active');
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container #menu-"+id[1]).slideDown('slow');		
+		$("#menu-container .homepage").slideUp('slow');
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container .about-section").slideDown('slow');
+		return false;
+	});
+
 	$(".main-menu a.projectbutton").click(function(){
 		$("#menu-container .content").slideUp('slow');
 		$("#menu-container .project-section").slideDown('slow');
